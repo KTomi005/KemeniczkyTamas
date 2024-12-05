4df4733d09c510f1c97cdd7a0ddec1fa2e04d99f
KTomi005 <kemtam197@hengersor.hu>
2024-12-05 13:23:22 +0100

    órai munka

---
+++ frontend/nodejs2/index.js
@@ -0,0 +1,13 @@
+
+// wrapper function
+// function(__dirname, __filename, module, require,  exports) {
+
+
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
+console.log(require);
+console.log(exports);
+
+
+//}
